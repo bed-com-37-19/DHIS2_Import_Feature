fc86b6860ca1b8358bb28ce71381f83c357347b9
BED_COM_37_19 <bed-com-37-19@unima.ac.mw>
2024-06-18 12:13:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dhis2_import_feature/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.dhis2_import_feature
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dhis2_import_feature.ui.theme.DHIS2_Import_FeatureTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DHIS2_Import_FeatureTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DHIS2_Import_FeatureTheme {
+        Greeting("Android")
+    }
+}
